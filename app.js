ac74bbc4bd01d0a05e96760ecc1d3179ffa293f6
Ishan <ishanatmuzaffarpur@gmail.com>
2014-09-10 16:44:07 +0530

    Registration created

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var fs = require('fs');
 var formidable = require('formidable');
 var util = require('util');
 var request = require('request');
+var mongo = require('mongodb');
 
 var staticServer = new(nodeStatic.Server)();
 var port = Number(process.env.PORT || 8080);
+
+// MongoHQ Server Url 'mongodb://ishanatmuz:m7382in@kahana.mongohq.com:10045/youthVibe2014';
+var mongoUri = process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/youthVibe2014';
 
 http.createServer(function (req, res) {
     // Testing for public folder for static hosting
@@ -23,113 +27,145 @@ http.createServer(function (req, res) {
     } else {
         // res.end('not inside public folder for my implementation');
         // console.log(parts.path);
-        if(parts.path == '/form') {
-            // Read the file and display it in here on console.
-            fs.readFile('form.html', function(error, data) {
-                if(error) {
-                    console.log('Error reading file :\n' + error);
-                } else {
-                    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-                    res.end(data);
-                }
-            });
-        } else if(parts.path == '/send') {
-            // start of my GCM Experiment
-            // console.log('inside send :\n');
-            // This only works for the get methods
-            // console.log(url.parse(req.url, true).query);
-            // Parse a form
-            var form = new formidable.IncomingForm();
-
-            form.parse(req, function(err, fields, files) {
-                var formData = util.inspect({fields: fields, files: files});
-                res.writeHead(200, {'content-type': 'text/plain'});
-                res.write('Received form:\n\n');
-                res.write('Fields :\nusername : ' + fields.username + '\npassword : ' + fields.password);
-                res.write('\n\nRaw Form Data : \n' + formData);
-                console.log(fields);
-                
-                // TODO: Have to remove the GCM firing from here and instead store the registration IDs in the mongoDB database.
-                // Will have to shift the GCM Experiment to a new location '/register'
-                // Start of my GCM experiment
-                var data = {
-                    "collapseKey": "applice",
-                    "delayWhileIdle": true,
-                    "timeToLive": 3,
-                    "data":{
-                        "message": "My message",
-                        "title": "My Title",
-                        "username": fields.username,
-                        "password": fields.password
-                    },
-                    // TODO: Will fetch these registration IDs from the database.
-                    "registration_ids":[fields.RegistrationID]
-                    };
-                
-                    var dataString =  JSON.stringify(data);
-                    var headers = {
-                        'Authorization' : 'key=AIzaSyBlii4ipb-Xzdybvg1DhdNcZ8oaU9-JfIk',
-                        'Content-Type' : 'application/json',
-                        'Content-Length' : dataString.length
-                    };
-
-                    var options = {
-                        host: 'android.googleapis.com',
-                        // host: 'requestb.in',
-                        port: 80,
-                        path: '/gcm/send',
-                        // path: '/11qa0tm1',
-                        method: 'POST',
-                        headers: headers
-                    };
-
-                    //Setup the request 
-                    var GCMreq = http.request(options, function(GCMres) {
-                        GCMres.setEncoding('utf-8');
-
-                        var responseString = '';
-
-                        GCMres.on('data', function(data) {
-                            // console.log('response data event');
-                            responseString += data;
-                        });
-
-                        GCMres.on('end', function() {
-                            // console.log('response end event');
-                            // var resultObject = JSON.parse(responseString);
-                            // print(responseString);
-                            console.log(util.inspect({response : responseString}));
-                            // console.log(resultObject);
-                            
-                            // Since we are writing to reponse from here. We have to end the response from here too
-                            res.write('\n\nGCM Data : \n' + util.inspect({response : responseString}));
-                            res.end();
-                        });
-                        console.log('STATUS: ' + GCMres.statusCode);
-                        console.log('HEADERS: ' + JSON.stringify(GCMres.headers));
-                    });
-
-                    GCMreq.on('error', function(e) {
-                        // TODO: handle error.
-                        console.log('error : ' + e.message + e.code);
-                    });
-
-                    GCMreq.write(dataString);
-                    GCMreq.end();
-
-                // End of my GCM Experiment
-                
-                
-                /*// requestb.in experiment
-                // var request = require('request');
-                var binurl ='http://requestb.in/11qa0tm1';
-                console.log('trying to send a request to : ' + binurl);
-                    request(binurl, function (error, response, body) {
-                    if (!error) {
-                        console.log(body);
+        
+        switch (parts.path) {
+            case '/form':
+                // Read the file and send it to the user
+                fs.readFile('form.html', function(error, data) {
+                    if(error) {
+                        console.log('Error reading file :\n' + error);
+                    } else {
+                        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+                        res.end(data);
                     }
-                });*/
-            });
+                });
+            break;
+            case '/send':
+                // start of my GCM Experiment
+                // console.log('inside send :\n');
+                // This only works for the get methods
+                // console.log(url.parse(req.url, true).query);
+                // Parse a form
+                var form = new formidable.IncomingForm();
+
+                form.parse(req, function(err, fields, files) {
+                    var formData = util.inspect({fields: fields, files: files});
+                    res.writeHead(200, {'content-type': 'text/plain'});
+                    res.write('Received form:\n\n');
+                    res.write('Fields :\nusername : ' + fields.username + '\npassword : ' + fields.password);
+                    res.write('\n\nRaw Form Data : \n' + formData);
+                    console.log(fields);
+
+                    // TODO: Have to remove the GCM firing from here and instead store the registration IDs in the mongoDB database.
+                    // Will have to shift the GCM Experiment to a new location '/send'
+                    // Start of my GCM experiment
+                    var data = {
+                        "collapseKey": "applice",
+                        "delayWhileIdle": true,
+                        "timeToLive": 3,
+                        "data":{
+                            "message": "My message",
+                            "title": "My Title",
+                            "username": fields.username,
+                            "password": fields.password
+                        },
+                        // TODO: Will fetch these registration IDs from the database.
+                        "registration_ids":[fields.RegistrationID]
+                        };
+
+                        var dataString =  JSON.stringify(data);
+                        var headers = {
+                            'Authorization' : 'key=AIzaSyBlii4ipb-Xzdybvg1DhdNcZ8oaU9-JfIk',
+                            'Content-Type' : 'application/json',
+                            'Content-Length' : dataString.length
+                        };
+
+                        var options = {
+                            host: 'android.googleapis.com',
+                            // host: 'requestb.in',
+                            port: 80,
+                            path: '/gcm/send',
+                            // path: '/11qa0tm1',
+                            method: 'POST',
+                            headers: headers
+                        };
+
+                        //Setup the request 
+                        var GCMreq = http.request(options, function(GCMres) {
+                            GCMres.setEncoding('utf-8');
+
+                            var responseString = '';
+
+                            GCMres.on('data', function(data) {
+                                // console.log('response data event');
+                                responseString += data;
+                            });
+
+                            GCMres.on('end', function() {
+                                // console.log('response end event');
+                                // var resultObject = JSON.parse(responseString);
+                                // print(responseString);
+                                console.log(util.inspect({response : responseString}));
+                                // console.log(resultObject);
+
+                                // Since we are writing to reponse from here. We have to end the response from here too
+                                res.write('\n\nGCM Data : \n' + util.inspect({response : responseString}));
+                                res.end();
+                            });
+                            console.log('STATUS: ' + GCMres.statusCode);
+                            console.log('HEADERS: ' + JSON.stringify(GCMres.headers));
+                        });
+
+                        GCMreq.on('error', function(e) {
+                            // TODO: handle error.
+                            console.log('error : ' + e.message + e.code);
+                        });
+
+                        GCMreq.write(dataString);
+                        GCMreq.end();
+
+                    // End of my GCM Experiment
+
+
+                    /*// requestb.in experiment
+                    // var request = require('request');
+                    var binurl ='http://requestb.in/11qa0tm1';
+                    console.log('trying to send a request to : ' + binurl);
+                        request(binurl, function (error, response, body) {
+                        if (!error) {
+                            console.log(body);
+                        }
+                    });*/
+                });
+            break;
+            case '/register':
+                // res.end('You opened the page for phone registration; which is meant to be accesed via the app or by the administrator.\nImplementation is remaining.');
+                var form = new formidable.IncomingForm();
+                form.parse(req, function(err, fields, files) {
+                    res.writeHead(200, {'content-type': 'text/plain'});
+                    if(fields.RegistrationID === undefined) {
+                        res.end('Oops Something went wrong. There was no registration ID detected for your device.\nPlease contact the administrators.');
+                    } else {
+                        // MongoDB server connection to store IDs
+                        mongo.Db.connect(mongoUri, function (err, db) {
+                            db.collection('userIds', function(err, collection) {
+                              collection.insert({'id': fields.RegistrationID}, {safe: true}, function(err, rs) {
+                                if(err) {
+                                  res.end("\nUser Registration Failed.\n" + err);
+                                } else if(rs) {
+                                  res.end("\nUser Registered successfully :\nRegistration ID : " + fields.RegistrationID + "\n\nResult : \n" + rs);
+                                }
+                              });
+                            });
+                          });
+                    }
+                });
+            break;
+            case '/users':
+                res.end('Template to display the list of current users');
+            break;
+            default:
+                console.log("Sorry, we are out of " + parts.path + ".");
         }
     }
 }).listen(port);
